48d0058b26b2cbf9bb2bbd001f573ab59880a5c7
fndos <fndos@hotmail.com>
2018-01-16 05:11:07 -0500

    nginx as proxy server (WIP)

--- microservice/microservice/settings.py
+++ microservice/microservice/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'newsfeed',
         'USER': 'root',
-        'PASSWORD': 'Aisakataiga1',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
